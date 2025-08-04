45a50b23b6517cfa1778a98b1b044a618386acfd
manasa <amanasa726@gmail.com>
2025-08-04 12:53:09 +0530

    statefile

---
+++ day-4-statefile-remote-config/main.tf
@@ -0,0 +1,32 @@
+
+# resource "aws_vpc" "dev" {
+#     cidr_block = "10.0.0.0/16"
+  
+# }
+
+# resource "aws_subnet" "dev-1" {
+#     cidr_block = "10.0.0.0/24"
+#      vpc_id = aws_vpc.dev.id
+# }
+
+# resource "aws_subnet" "test" {
+#     cidr_block = "10.0.1.0/24"
+#      vpc_id = aws_vpc.dev.id
+
+# }
+ resource "aws_instance" "name" {
+     ami = "ami-002f6e91abff6eb96"
+     instance_type = "t2.micro"
+  
+ }
+# resource "aws_subnet" "devops" {
+#     cidr_block = "10.0.3.0/24"
+#      vpc_id = aws_vpc.dev.id
+# }
+
+# resource "aws_subnet" "prod" {
+#     cidr_block = "10.0.2.0/24"
+#      vpc_id = aws_vpc.dev.id
+  
+# }
+
