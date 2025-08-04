f9cf1740ce2a2ae88bd2c00088b85f8590791e89
manasa <amanasa726@gmail.com>
2025-08-04 11:44:53 +0530

    dfghjk

---
+++ day-8-vpc-module/module.tf
@@ -0,0 +1,9 @@
+
+ module "vpc" {
+  source             = "../day-8-resource-cust-vpc-nt"
+  vpc_name           = "my-vpc"
+  vpc_cidr_block     = "10.0.0.0/16"
+  public_subnets     = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnets    = ["10.0.2.0/24", "10.0.3.0/24"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+}
