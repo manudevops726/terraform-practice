d19c57b07a0f837eb26902f79d5d38e1ce1b7298
manasa <amanasa726@gmail.com>
2025-08-08 13:04:34 +0530

    seedjs

---
+++ zepto-project-block/zepto-backend/src/seed.js
@@ -0,0 +1,22 @@
+const { Product, sequelize } = require('./models');
+
+async function seed() {
+  await sequelize.sync({ force: false }); // ensure tables exist, but don't drop data
+
+  const products = [
+    { name: 'Banana', image: 'https://link-to-banana-image.jpg', price: 10 },
+    { name: 'Cranberries', image: 'https://link-to-cranberries.jpg', price: 25 },
+    { name: 'Spinach', image: 'https://link-to-spinach.jpg', price: 15 },
+    { name: 'Tomato', image: 'https://link-to-tomato.jpg', price: 12 },
+    { name: 'Rose', image: 'https://link-to-rose-image.jpg', price: 20 }
+  ];
+
+  for (const product of products) {
+    await Product.findOrCreate({ where: { name: product.name }, defaults: product });
+  }
+
+  console.log('Seeding done!');
+  process.exit();
+}
+
+seed().catch(console.error);
