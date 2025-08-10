caffc53ce29a51afebd2e470d96fb30f59448bd6
manudevops726 <amanasa726@gmail.com>
2025-08-10 13:59:11 +0530

    Update api.js

--- zepto-project-block/zepto-frontend/src/api.js
+++ zepto-project-block/zepto-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://<YOUR_BACKEND_IP>:3001'  // 🔁 Replace with actual IP or domain
+  baseURL: 'http://54.242.110.46:3001'  // 🔁 Replace with actual IP or domain
 });
 
 export default api;
