95b75f3920f2466e3f4a85534d0f573d9a37635a
manudevops726 <amanasa726@gmail.com>
2025-08-10 22:51:47 +0530

    Update seed.js

--- zepto-project-block/zepto-backend/src/seed.js
+++ zepto-project-block/zepto-backend/src/seed.js
@@ -4,11 +4,11 @@ async function seed() {
   await sequelize.sync({ force: false }); // ensure tables exist, but don't drop data
 
   const products = [
-    { name: 'Banana', image: 'https://images.heb.com/is/image/HEBGrocery/000377497', price: 10 },
-    { name: 'Cranberries', image: 'https://link-to-cranberries.jpg', price: 25 },
-    { name: 'Spinach', image: 'https://link-to-spinach.jpg', price: 15 },
-    { name: 'Tomato', image: 'https://link-to-tomato.jpg', price: 12 },
-    { name: 'Rose', image: 'https://link-to-rose-image.jpg', price: 20 }
+    { name: 'Banana', image: 'https://images.heb.com/is/image/HEBGrocery/000377497', price: 70 },
+    { name: 'Cranberries', image: 'https://i.pinimg.com/originals/47/6c/f3/476cf3fce6bdddedd21e887ef9fcf0ab.jpg', price: 125 },
+    { name: 'Spinach', image: 'https://lettuceinfo.org/wp-content/uploads/2020/09/Spinach.jpg', price: 45 },
+    { name: 'Raddish', image: 'https://img.freepik.com/premium-photo/daikon_1092848-380.jpg', price: 50 },
+    { name: 'Rose', image: 'https://assets-news.housing.com/news/wp-content/uploads/2022/11/25150143/1-18.jpg', price: 100 }
   ];
 
   for (const product of products) {
